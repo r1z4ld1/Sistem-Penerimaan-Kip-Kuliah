3002719923d6404c21d3db6c8188f2e153fea72e
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-14 23:06:37 +0800

    modified file routes add url 3 roles

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth'])
-    ->name('dashboard');
+//route untuk admin
+Route::middleware(['auth', 'role:admin'])
+    ->prefix('dashboard/admin')
+    ->name('admin.')
+    ->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+        Route::get('/', [DashboardController::class, 'admin'])
+            ->name('dashboard');
+    });
+
+//route untuk mahasiswa
+Route::middleware(['auth', 'role:mahasiswa'])
+    ->prefix('dashboard/mahasiswa')
+    ->name('mahasiswa.')
+    ->group(function () {
+
+        Route::get('/', [DashboardController::class, 'mahasiswa'])
+            ->name('dashboard');
+    });
+
+//route untuk verifikator
+Route::middleware(['auth', 'role:verifikator'])
+    ->prefix('dashboard/verifikator')
+    ->name('verifikator.')
+    ->group(function () {
+
+        Route::get('/', [DashboardController::class, 'verifikator'])
+            ->name('dashboard');
+    });
 
 require __DIR__ . '/auth.php';
