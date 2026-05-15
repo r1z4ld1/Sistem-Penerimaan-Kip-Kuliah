f59b68f44c49cd51f3dbe27befd1bc21c819679f
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-15 16:48:31 +0800

    add file jurusan requests

---
+++ app/Http/Requests/JurusanUpdateRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class JurusanUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'universitas_id' => 'required|exists:universitas,id',
+
+            'nama_jurusan' => 'required'
+        ];
+    }
+}
