25ec8527415ff9985b2d128a7550fe17242524f5
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-15 16:07:43 +0800

    add file UniversitasController

---
+++ app/Http/Controllers/Admin/UniversitasController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Universitas;
+use App\Http\Requests\UniversitasStoreRequest;
+use App\Http\Requests\UniversitasUpdateRequest;
+use App\Services\UniversitasService;
+
+class UniversitasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    protected $service;
+
+    public function __construct(UniversitasService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        $query = Universitas::query();
+
+        if ($request->search) {
+
+            $query->where(
+                'nama_universitas',
+                'like',
+                '%' . $request->search . '%'
+            );
+        }
+
+        $universitas = $query
+            ->latest()
+            ->paginate(10);
+
+        return view(
+            'dashboard.admin.universitas.index',
+            compact('universitas')
+        );
+    }
+
+    public function create()
+    {
+        return view('dashboard.admin.universitas.create');
+    }
+
+    public function store(UniversitasStoreRequest $request)
+    {
+        $this->service->store($request->validated());
+
+        return redirect()
+            ->route('admin.universitas.index')
+            ->with('success', 'Data universitas berhasil ditambahkan');
+    }
+
+    public function edit(Universitas $universitas)
+    {
+        return view(
+            'dashboard.admin.universitas.edit',
+            compact('universitas')
+        );
+    }
+
+    public function update(
+        UniversitasUpdateRequest $request,
+        Universitas $universitas
+    ) {
+
+        $this->service->update(
+            $universitas,
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('admin.universitas.index')
+            ->with('success', 'Data universitas berhasil diupdate');
+    }
+
+    public function destroy(Universitas $universitas)
+    {
+        $this->service->delete($universitas);
+
+        return redirect()
+            ->route('admin.universitas.index')
+            ->with('success', 'Data universitas berhasil dihapus');
+    }
+}
