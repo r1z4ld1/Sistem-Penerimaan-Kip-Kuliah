66525520c7a9bf0a02a173c482ee787cca32bbc8
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-19 21:27:17 +0800

    update route role mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\MahasiswaController;
+use App\Http\Controllers\Admin\UniversitasController;
+use App\Http\Controllers\Admin\JurusanController;
+use App\Http\Controllers\Mahasiswa\PendaftaranController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//route untuk admin
+//route untuk role admin
 Route::middleware(['auth', 'role:admin'])
     ->prefix('dashboard/admin')
     ->name('admin.')
@@ -19,9 +22,17 @@ Route::middleware(['auth', 'role:admin'])
             ->name('dashboard');
 
         Route::resource('mahasiswa', MahasiswaController::class);
+        Route::resource('universitas', UniversitasController::class)
+            ->parameters([
+                'universitas' => 'universitas'
+            ]);
+        Route::resource('jurusan', JurusanController::class)
+            ->parameters([
+                'jurusan' => 'jurusan'
+            ]);
     });
 
-//route untuk mahasiswa
+//route untuk role mahasiswa
 Route::middleware(['auth', 'role:mahasiswa'])
     ->prefix('dashboard/mahasiswa')
     ->name('mahasiswa.')
@@ -29,9 +40,20 @@ Route::middleware(['auth', 'role:mahasiswa'])
 
         Route::get('/', [DashboardController::class, 'mahasiswa'])
             ->name('dashboard');
+
+        Route::resource('pendaftaran', PendaftaranController::class)
+            ->parameters([
+                'pendaftaran' => 'pendaftaran'
+            ]);
+
+        // route untuk mengambil jurusan berdasarkan universitas
+        Route::get(
+            'get-jurusan/{universitas_id}',
+            [PendaftaranController::class, 'getJurusan']
+        )->name('mahasiswa.getJurusan');
     });
 
-//route untuk verifikator
+//route untuk role verifikator
 Route::middleware(['auth', 'role:verifikator'])
     ->prefix('dashboard/verifikator')
     ->name('verifikator.')
