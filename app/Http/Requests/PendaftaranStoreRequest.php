02070812624bdf7e937bccbd8bd3c061cc9c9adc
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-19 21:21:23 +0800

    add file pendaftaran requests

---
+++ app/Http/Requests/PendaftaranStoreRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PendaftaranStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+
+            'universitas_id' => 'required|exists:universitas,id',
+
+            'jurusan_id' => 'required|exists:jurusan,id',
+
+        ];
+    }
+}
