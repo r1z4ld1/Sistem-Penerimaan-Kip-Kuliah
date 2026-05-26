010395c2dfb3a240a191e6c67330f74585495475
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-26 12:51:19 +0800

    add file UserController

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+
+use App\Services\UserService;
+
+use App\Http\Controllers\Controller;
+
+use Spatie\Permission\Models\Role;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
+
+class UserController extends Controller
+{
+    protected $service;
+
+    public function __construct(
+        UserService $service
+    ) {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        $users = $this->service
+            ->getAll($request->search);
+
+        return view(
+            'dashboard.admin.users.index',
+            compact('users')
+        );
+    }
+
+    public function create()
+    {
+        $roles = Role::all();
+
+        return view(
+            'dashboard.admin.users.create',
+            compact('roles')
+        );
+    }
+
+    public function store(
+        UserStoreRequest $request
+    ) {
+
+        $this->service->store(
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('admin.users.index')
+            ->with(
+                'success',
+                'User berhasil ditambahkan'
+            );
+    }
+
+    public function edit(User $user)
+    {
+        $roles = Role::all();
+
+        return view(
+            'dashboard.admin.users.edit',
+            compact('user', 'roles')
+        );
+    }
+
+    public function update(
+        UserUpdateRequest $request,
+        User $user
+    ) {
+
+        $this->service->update(
+            $user,
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('admin.users.index')
+            ->with(
+                'success',
+                'User berhasil diupdate'
+            );
+    }
+
+    public function destroy(User $user)
+    {
+        $deleted = $this->service
+            ->delete($user);
+
+        if (!$deleted) {
+
+            return redirect()
+                ->back()
+                ->with(
+                    'error',
+                    'Anda tidak bisa menghapus akun sendiri'
+                );
+        }
+
+        return redirect()
+            ->route('admin.users.index')
+            ->with(
+                'success',
+                'User berhasil dihapus'
+            );
+    }
+}
