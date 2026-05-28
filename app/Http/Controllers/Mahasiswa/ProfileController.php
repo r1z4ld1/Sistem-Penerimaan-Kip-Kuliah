e0e529e2fcb62d617e1dc3e6379f7a3aea47c6fc
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-29 00:23:56 +0800

    add file ProfileController

---
+++ app/Http/Controllers/Mahasiswa/ProfileController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Mahasiswa;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use App\Services\MahasiswaService;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\MahasiswaStoreRequest;
+use App\Http\Requests\MahasiswaUpdateRequest;
+
+class ProfileController extends Controller
+{
+    protected $service;
+
+    public function __construct(
+        MahasiswaService $service
+    ) {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::where(
+            'user_id',
+            Auth::id()
+        )->first();
+
+        return view(
+            'dashboard.mahasiswa.profile.index',
+            compact('mahasiswa')
+        );
+    }
+
+    public function store(
+        MahasiswaStoreRequest $request
+    ) {
+
+        $data = $request->validated();
+
+        $data['user_id'] = Auth::id();
+
+        $this->service->store($data);
+
+        return redirect()
+            ->route('mahasiswa.profile.index')
+            ->with(
+                'success',
+                'Biodata berhasil dilengkapi'
+            );
+    }
+
+    public function update(
+        MahasiswaUpdateRequest $request,
+        Mahasiswa $mahasiswa
+    ) {
+
+        if ($mahasiswa->user_id != Auth::id()) {
+            abort(403);
+        }
+
+        $this->service->update(
+            $mahasiswa,
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('mahasiswa.profile.index')
+            ->with(
+                'success',
+                'Biodata berhasil diupdate'
+            );
+    }
+}
