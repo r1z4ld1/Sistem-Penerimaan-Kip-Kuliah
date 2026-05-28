197e7b8dc17737cd6ec79235388806df74042d8d
r1z4ld1 <linuxbacktrack737@gmail.com>
2026-05-28 22:16:05 +0800

    add file MahasiswaRepository in folder verifikator

---
+++ app/Repositories/Verifikator/MahasiswaRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories\Verifikator;
+
+use App\Models\Mahasiswa;
+
+class MahasiswaRepository
+{
+    public function getAll(?string $search = null)
+    {
+        return Mahasiswa::query()
+
+            ->when($search, function ($query) use ($search) {
+
+                $query->where('nama_lengkap', 'like', "%{$search}%")
+                    ->orWhere('nik', 'like', "%{$search}%")
+                    ->orWhere('nisn', 'like', "%{$search}%");
+            })
+
+            ->latest()
+            ->paginate(10);
+    }
+
+    public function findById(int $id)
+    {
+        return Mahasiswa::findOrFail($id);
+    }
+}
